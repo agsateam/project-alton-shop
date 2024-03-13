ffc71ffd53ddcddef339c377bf758a1aee8a86d0
vardrz <fatkhurrozakf@gmail.com>
2024-03-13 22:48:15 +0700

    chore: init liveware & daisyui

---
+++ resources/views/components/home/navbar.blade.php
@@ -0,0 +1,46 @@
+<div class="navbar bg-primary text-white">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </div>
+        {{-- navbar untuk responsif nanti --}}
+        <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a>Item 1</a></li>
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+        {{-- end --}}
+      </div>
+      <a class="font-bold text-xl ml-3">ALTON</a>
+      <div class="ml-5 hidden lg:flex">
+        <ul class="menu menu-horizontal">
+          <li><a>Baby & Kids</a></li>
+          <li><a>Atasan</a></li>
+          <li><a>Etc</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="navbar-end mr-3">
+        <button class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span class="badge badge-sm indicator-item">8</span>
+            </div>
+        </div>
+        <a class="avatar ml-3" href="login" wire:navigate>
+            <div class="w-8">
+                <img src="https://cdn-icons-png.flaticon.com/512/6596/6596121.png">
+            </div>
+        </a>
+    </div>
+  </div>
