040273e8ddf6f68647fa7f26547402538b9df635
ikkromm18 <muhammadikkrom749@gmail.com>
2024-04-04 12:12:03 +0700

    feat:Fitur Promo dan Product Section. fix:Swiper JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,55 +12,54 @@ import "swiper/css/pagination";
 
 livewire_hot_reload();
 
-// Swiper JS
-const swiper = new Swiper(".swiper", {
-    modules: [Navigation, Pagination],
-    grabCursor: true,
-    // centeredSlides: true,
-    loop: true,
-    spaceBetween: 20,
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-});
-
-// Function Responsive Swiper
-function responsiveSlidePerView() {
-    if (window.innerWidth >= 1024) {
-        // PC
-        swiper.params.slidesPerView = 5;
-    } else if (window.innerWidth >= 840) {
-        swiper.params.slidesPerView = 4;
-    } else if (window.innerWidth >= 560) {
-        swiper.params.slidesPerView = 3;
-    } else if (window.innerWidth >= 425) {
-        swiper.params.slidesPerView = 2;
-    } else {
-        // Mobile
-        swiper.params.slidesPerView = 1;
-    }
-    swiper.update(); // Update swiper instance
-}
-window.onload = responsiveSlidePerView; // Update on page load
-window.addEventListener("resize", responsiveSlidePerView); // Update on window resize
-// End Swiper JS
-
 document.addEventListener("livewire:navigated", () => {
     initFlowbite();
+    // Swiper JS
+    const swiper = new Swiper(".swiper", {
+        modules: [Navigation, Pagination],
+        grabCursor: true,
+        // centeredSlides: true,
+        loop: true,
+        spaceBetween: 20,
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+    });
+
+    // Function Responsive Swiper
+    function responsiveSlidePerView() {
+        if (window.innerWidth >= 1024) {
+            swiper.params.slidesPerView = 5;
+        } else if (window.innerWidth >= 840) {
+            swiper.params.slidesPerView = 4;
+        } else if (window.innerWidth >= 560) {
+            swiper.params.slidesPerView = 3;
+        } else if (window.innerWidth >= 425) {
+            swiper.params.slidesPerView = 2;
+        } else {
+            swiper.params.slidesPerView = 1;
+        }
+        swiper.update(); // Update swiper instance
+    }
+    responsiveSlidePerView();
+
+    window.onload = responsiveSlidePerView; // Update on page load
+    window.addEventListener("resize", responsiveSlidePerView); // Update on window resize
+    // End Swiper JS
 });
