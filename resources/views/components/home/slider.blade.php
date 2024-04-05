b813e366afcdd86376502f38ae339b6ca046ae42
vardrz <fatkhurrozakf@gmail.com>
2024-04-05 10:48:09 +0700

    fix: responsive search bar, slider

--- resources/views/components/home/slider.blade.php
+++ resources/views/components/home/slider.blade.php
@@ -10,10 +10,10 @@ $last = count($slider);
 
 {{-- Flowbite Carousel --}}
 <div id="default-carousel" class="relative w-full bg-primary" data-carousel="slide">
-  <div class="relative h-40 overflow-hidden sm:h-64 md:h-80 lg:h-[450px]">
+  <div class="relative h-44 overflow-hidden sm:h-64 md:h-80 lg:h-[450px]">
       @foreach($slider as $item)
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src="{{ $item }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+      <div class="absolute top-0 hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="{{ $item }}" class="w-full" alt="...">
       </div>
       @endforeach
   </div>
