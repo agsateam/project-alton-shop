ed7666f62045d47b9006d3ebc2466344ef450941
vardrz <fatkhurrozakf@gmail.com>
2024-04-05 11:13:47 +0700

    fix: sidebar active button dosnt work on https

--- resources/views/components/sidebar/button.blade.php
+++ resources/views/components/sidebar/button.blade.php
@@ -1,5 +1,9 @@
+<?php 
+$checkPath = request()->getPathInfo() == $href;
+?>
+
 <a {{ $attributes }}
-    class="{{ request()->fullUrlIs(url($href)) ? "btn-primary text-white" : "btn-ghost text-black hover:bg-primary hover:text-white "}}
+    class="{{ $checkPath ? "btn-primary text-white" : "btn-ghost text-black hover:bg-primary hover:text-white "}}
         btn w-full mb-1 md:mb-0 md:my-2 text-start justify-start text-xs md:text-base"
 >
     {{ $slot }}
