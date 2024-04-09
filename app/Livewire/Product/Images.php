d966c276965e9ad52a0957070f74147310767077
vardrz <fatkhurrozakf@gmail.com>
2024-04-09 13:37:35 +0700

    fix: implement product detail image selected with js

--- app/Livewire/Product/Images.php
+++ app/Livewire/Product/Images.php
@@ -7,17 +7,9 @@ use Livewire\Component;
 class Images extends Component
 {
     public $images = [];
-    public $mainImages = '';
 
     public function render()
     {
-        $this->mainImages == '' ? $this->changeMainImages($this->images[0]) : [];
-
         return view('livewire.product.images');
-    }
-
-    public function changeMainImages($image)
-    {
-        $this->mainImages = $image;
     }
 }
