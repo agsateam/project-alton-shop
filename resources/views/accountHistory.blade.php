2278bed018b4f03690b15f8652a59809429e1514
Anoxta99 <wongklowor99@gmail.com>
2024-04-22 12:13:43 +0700

    feat: add tabel history, avatar, change avatar modal

--- resources/views/accountHistory.blade.php
+++ resources/views/accountHistory.blade.php
@@ -2,16 +2,16 @@
   <x-master.accountLayout>
     {{-- content account --}}
     <div class="container md:px-3 md:py-5 mx-auto">
-      <div class="flex md:flex-wrap md:-m-4">
+      <div class="flex md:flex-wrap md:-m-4 justify-between">
         <div class="p-2 w-1/4 md:flex md:flex-wrap">
-          <div class="border-2 relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg ">
+          <div class="relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg ">
             <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
             <h2 class="title-font font-bold text-2xl md:text-5xl">7</h2>
             <span class="text-xs ml-2 mb-1 md:ml-4 md:mb-3 text-slate-200 md:text-md md:text-xl leading-relaxed absolute bottom-0 left-0">Pending</p>
           </div>
         </div>
         <div class="p-2 w-1/4 md:flex md:flex-wrap">
-          <div class="border-2 relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg text-sky-500">
+          <div class="relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg text-sky-500">
             <svg class="text-sky-500 mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
             </svg>            
@@ -20,21 +20,43 @@
           </div>
         </div>
         <div class="p-2 w-1/4 md:flex md:flex-wrap">
-          <div class="border-2 relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" viewBox="0 0 24 24" id="done"><g><g><path d="M16.62 6.21a1 1 0 0 0-1.41.17l-7 9-3.43-4.18a1 1 0 1 0-1.56 1.25l4.17 5.18a1 1 0 0 0 .78.37 1 1 0 0 0 .83-.38l7.83-10a1 1 0 0 0-.21-1.41zm5 0a1 1 0 0 0-1.41.17l-7 9-.61-.75-1.26 1.62 1.1 1.37a1 1 0 0 0 .78.37 1 1 0 0 0 .78-.38l7.83-10a1 1 0 0 0-.21-1.4z"></path><path d="M8.71 13.06 10 11.44l-.2-.24a1 1 0 0 0-1.43-.2 1 1 0 0 0-.15 1.41z"></path></g></g></svg>
+          <div class="relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" viewBox="0 0 24 24"><g><g><path d="M16.62 6.21a1 1 0 0 0-1.41.17l-7 9-3.43-4.18a1 1 0 1 0-1.56 1.25l4.17 5.18a1 1 0 0 0 .78.37 1 1 0 0 0 .83-.38l7.83-10a1 1 0 0 0-.21-1.41zm5 0a1 1 0 0 0-1.41.17l-7 9-.61-.75-1.26 1.62 1.1 1.37a1 1 0 0 0 .78.37 1 1 0 0 0 .78-.38l7.83-10a1 1 0 0 0-.21-1.4z"></path><path d="M8.71 13.06 10 11.44l-.2-.24a1 1 0 0 0-1.43-.2 1 1 0 0 0-.15 1.41z"></path></g></g></svg>
             <h2 class="title-font font-bold text-2xl md:text-5xl">12</h2>
             <span class="text-xs ml-2 mb-1 md:ml-4 md:mb-3 text-slate-200 md:text-md md:text-xl leading-relaxed absolute bottom-0 left-0">Completed</p>
           </div>
         </div>
         <div class="p-2 w-1/4 md:flex md:flex-wrap">
-          <div class="border-2 relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" viewBox="0 0 101 101" id="cancel"><path d="M50.5 84.6c18.8 0 34.1-15.3 34.1-34.1S69.3 16.4 50.5 16.4 16.4 31.7 16.4 50.5s15.3 34.1 34.1 34.1zm0-63.4c16.1 0 29.3 13.1 29.3 29.3S66.6 79.8 50.5 79.8 21.2 66.6 21.2 50.5s13.2-29.3 29.3-29.3z"></path><path d="M37.7 63c.5.5 1.1.7 1.7.7s1.2-.2 1.7-.7l9.4-9.4 9.4 9.4c.5.5 1.1.7 1.7.7s1.2-.2 1.7-.7c.9-.9.9-2.5 0-3.4l-9.4-9.4 9.4-9.4c.9-.9.9-2.5 0-3.4s-2.5-.9-3.4 0l-9.4 9.4-9.4-9.4c-.9-.9-2.5-.9-3.4 0s-.9 2.5 0 3.4l9.4 9.4-9.4 9.4c-.9.9-.9 2.4 0 3.4z"></path></svg>
+          <div class="relative h-20 w-34 md:h-32 md:w-52 border-primary shadow-lg px-2 py-1 md:px-4 md:py-6 rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 md:mr-4 h-6 w-6 md:h-12 md:w-12 absolute right-0 top-1/4 md:top-1/2 transform -translate-y-1/2" viewBox="0 0 101 101"><path d="M50.5 84.6c18.8 0 34.1-15.3 34.1-34.1S69.3 16.4 50.5 16.4 16.4 31.7 16.4 50.5s15.3 34.1 34.1 34.1zm0-63.4c16.1 0 29.3 13.1 29.3 29.3S66.6 79.8 50.5 79.8 21.2 66.6 21.2 50.5s13.2-29.3 29.3-29.3z"></path><path d="M37.7 63c.5.5 1.1.7 1.7.7s1.2-.2 1.7-.7l9.4-9.4 9.4 9.4c.5.5 1.1.7 1.7.7s1.2-.2 1.7-.7c.9-.9.9-2.5 0-3.4l-9.4-9.4 9.4-9.4c.9-.9.9-2.5 0-3.4s-2.5-.9-3.4 0l-9.4 9.4-9.4-9.4c-.9-.9-2.5-.9-3.4 0s-.9 2.5 0 3.4l9.4 9.4-9.4 9.4c-.9.9-.9 2.4 0 3.4z"></path></svg>
             <h2 class="title-font font-bold text-2xl md:text-5xl">9</h2>
-            <span class="text-xs ml-2 mb-1 md:ml-4 md:mb-3 text-slate-200 md:text-md md:text-xl leading-relaxed absolute bottom-0 left-0">Pending</p>
+            <span class="text-xs ml-2 mb-1 md:ml-4 md:mb-3 text-slate-200 md:text-md md:text-xl leading-relaxed absolute bottom-0 left-0">Canceled</p>
           </div>
         </div>
       </div>
-      <div class="flex md:flex-wrap md:-m-4">
+      <div class="flex flex com w-full my-8">
+        <div class="shadow-xl h-full w-full px-2 py-1 rounded-xl">
+          <ul class="flex flex-wrap border-b-2 border-gray-200 w-full justify-around text-md font-medium text-center" id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
+            <li role="presentation">
+                <button class="inline-block p-4 hover:border-b-2 text-primary font-semibold hover:border-gray-300" id="all-tab" data-tabs-target="#all" type="button" role="tab" aria-controls="all" aria-selected="false">All</button>
+            </li>
+            <li role="presentation">
+                <button class="inline-block p-4 hover:border-b-2 text-primary font-semibold hover:border-gray-300" id="delivery-tab" data-tabs-target="#delivery" type="button" role="tab" aria-controls="delivery" aria-selected="false">Delivery</button>
+            </li>
+            <li role="presentation">
+                <button class="inline-block p-4 hover:border-b-2 text-primary font-semibold hover:border-gray-300" id="completed-tab" data-tabs-target="#completed" type="button" role="tab" aria-controls="completed" aria-selected="false">Completed</button>
+            </li>
+            <li role="presentation">
+                <button class="inline-block p-4 hover:border-b-2 text-primary font-semibold hover:border-gray-300" id="pending-tab" data-tabs-target="#pending" type="button" role="tab" aria-controls="pending" aria-selected="false">Pending</button>
+            </li>
+            <li role="presentation">
+                <button class="inline-block p-4 hover:border-b-2 text-primary font-semibold hover:border-gray-300" id="canceled-tab" data-tabs-target="#canceled" type="button" role="tab" aria-controls="canceled" aria-selected="false">Canceled</button>
+            </li>
+          </ul>
+          <div id="default-tab-content">
+            
+          </div>
+        </div>
       </div>
     </div>            
     {{-- end content account --}}
--- resources/views/components/master/accountLayout.blade.php
+++ resources/views/components/master/accountLayout.blade.php
@@ -2,7 +2,23 @@
   <div class="relative mt-0 md:mt-5 w-full md:w-1/4 md:h-screen md:pl-4">
       <div class="card w-50 bg-base-100 shadow-md relative">
           <figure class="px-5 pt-2">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNWQQdRQ1JIuiKNvTqBukJH4WugmOWF7A_-w&s" title="Foto Profil" alt="Photo" class="rounded-xl" style="width: 200px; height: 200px;" />
+            <div class="avatar relative group/item">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNWQQdRQ1JIuiKNvTqBukJH4WugmOWF7A_-w&s" title="Foto Profil" alt="Photo" class="rounded-full" style="width: 200px; height: 200px;" />
+              <button class="group/edit invisible group-hover/item:visible btn btn-square btn-ghost mr-1 -bottom-2 right-0 absolute hover:bg-transparent" onclick="change_pict.showModal()">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>              
+              </button>
+              <dialog id="change_pict" class="modal">
+                <div class="modal-box">
+                  <form method="dialog">
+                    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                  </form>
+                  <h3 class="font-bold text-lg py-5" style="align-items: center; justify-content: center; display:flex; flex-direction: column">Ukuran max. 10MB</h3>
+                  <a href="" class="btn btn-square mx-auto bg-primary text-white hover:bg-white hover:text-black hover:border-2 hover:border-primary w-40 h-70" style="display: flex; align-items: center; justify-content: center; text-align: center;">Upload Foto</a>
+                </div>
+              </dialog>                                          
+            </div>
           </figure>
           <div class="card-body items-center text-center">
             <h1 class="card-title">
@@ -19,6 +35,7 @@
   <div class="flex flex-col w-full md:flex-row md:p-2 mt-2">
       {{ $slot }}
   </div>
+  
   <div class="md:hidden card shadow-md mt-4 md:mt-8 md:flex-col"><a class="btn btn-primary text-white w-full mb-1 md:mb-0 md:mt-2 text-start justify text-xs md:text-base hover:bg-white hover:text-black" href="/logout" wire:navigate>Log Out</a class=""></div>
 </div>
 
